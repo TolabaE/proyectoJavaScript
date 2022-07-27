dd8a3c104540d3fdbb89405594c172fd7f61b4d1
Eduardo Tolaba <eduardoetolaba@gmail.com>
2022-07-27 20:53:13 -0300

    trabajo de JS sobre funciones

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-let totales = 0;
-for (let i = 1;i <6; i+= 1){
-    let horas = parseInt(prompt("ingrese horas trabajadas por dias"));
-    totales += horas;
+function talle(numero) {
+    if((numero>1)&(numero<21.5)){
+        alert("la medida ingresada no pertenece a ninguna talla")
+    }
+    else if((numero>= 21.6)&(numero<=22.5)){
+        console.log("tu talla es 34")
+    }
+    else if((numero>= 22.6)&(numero<=23.2)){
+        console.log("tu talla es 35")
+    }
+    else if((numero>= 23.3)&(numero<=23.8)){
+        console.log("tu talla es 36")
+    }
+    else if((numero>= 23.9)&(numero<=24.5)){
+        console.log("tu talla es 37")
+    }
+    else if ((numero>= 24.6)&(numero<=25.3)) {
+        console.log("tu talla es 38");
+    }
+    else if ((numero>= 25.4)&(numero<=25.9)){
+        console.log("tu talla es 39");
+    }
+    else if ((numero>= 26)&(numero<=26.5)){
+        console.log("tu talla es 40");
+    }
+    else if ((numero>= 26.6)&(numero<=27.9)){
+        console.log("tu talla es 41");
+    }
+    else if((numero>= 28)&(numero<=28.9)){
+        console.log("tu talla es 34")
+    }else if(numero>29){
+        alert("la medida ingresada excede la talla")
+    }
 }
-console.log(`por la cantidad de horas trabajadas de lunes a viernes obtienes sueldo de: $ ${totales*600}.`);
+let medida;
+let resultado;
 
+do {
+    medida = parseFloat(prompt(`para su saber talle:\n\n # ingrese la medida de su pie \n # o presione 0 si ya sabe`));
 
+    resultado = talle(medida);
+} while (medida!= 0);
+console.log(resultado);
