819269df8e38acf94eba213fc4780d099979f7c5
Eduardo Tolaba <eduardoetolaba@gmail.com>
2022-08-03 22:14:32 -0300

    trabajo de la primera preentrega del proyecto final

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-
+//este codigo te permite comprar carros ingresando una cantidad de plata,podes comprarlo todos o de a uno.
 class Vehiculos{
     constructor(modelo,motor,velocidad,precio,){
         this.modelo = modelo;
@@ -9,30 +8,57 @@ class Vehiculos{
     }
 }
 
+const bugatti = new Vehiculos ("bugatti","Chiron","407 Km/h",870000);
+const McLaren = new Vehiculos ("McLaren","MP412C","326 Km/h",380000);
+const ferrari = new Vehiculos ("ferrari","599 GTE","335 Km/h",275000);
+const lamborghini = new Vehiculos ("lamborghini","aventador","350Km/h",550000);
 
-const bugatti = new Vehiculos ("bugatti","Chiron","407 Km/h",2700000);
-const McLaren = new Vehiculos ("McLaren","MP412C","326 Km/h",300000);
-const ferrari = new Vehiculos ("ferrari","599 GTE","335 Km/h",675000);
-const lamborghini = new Vehiculos ("lamborghini","aventador","350Km/h",400000);
+const autosCarrera = [bugatti,McLaren,ferrari,lamborghini];
 
-const autosCarrera = [lamborghini,ferrari,McLaren,bugatti];
+function compra(plata = 0,precio = 0){
+    let vuelto = plata - precio;
+    if(vuelto>0){
+        console.log(`su compra fue exitosa,le sobra $ ${vuelto}.`);
+    }else if(vuelto<0){
+        console.log("el dinero ingresado no es suficiente,eliga otro carro mas barato");
+    }else{
+        console.log("gracias por su compra,puede seguir comprando")
+    }
+}
 
 let jugador,coche;
+
 do {
     alert("elija un coche para comprar");
-    coche = parseFloat(prompt(`selecione la opcion de acuerdo al numero \n 1-bugatti \n 2-McLaren \n 3- ferrari \n 4- lamborghini \n 5-finalizar compra`));
+    coche = parseFloat(prompt(`selecione la opcion de acuerdo al numero \n 1-bugatti \n 2-McLaren \n 3- ferrari \n 4- lamborghini \n 5-comprar todos los autos \n 6-finalizar compra`));
     if(coche ==1){
-        console.table(autosCarrera[0]);
-    }else if (coche ==2){
+        console.table(autosCarrera[0])
+        jugador = parseFloat(prompt("ingrese el monto para comprarlo"));
+        compra(jugador,bugatti.precio);
+    }else if (coche == 2){
         console.table(autosCarrera[1]);
-    }else if (coche ==3){
+        jugador = parseFloat(prompt("ingrese el monto para comprarlo"));
+        compra(jugador,McLaren.precio);
+    }else if (coche == 3){
         console.table(autosCarrera[2]);
+        jugador = parseFloat(prompt("ingrese el monto para comprarlo"));
+        compra(jugador,ferrari.precio);
     }else if (coche == 4){
         console.table(autosCarrera[3]);
-    }else if (coche != 1,2,3,4,5){
+        jugador = parseFloat(prompt("ingrese el monto para comprarlo"));
+        compra(jugador,lamborghini.precio);
+    }else if (coche == 5){
+        const sumaTotal = autosCarrera.map(autosarray => autosarray.precio);
+        console.log(sumaTotal.reduce((pre,act)=> pre + act,0));
+        jugador = parseFloat(prompt("ingrese el monto total para comprar todos"));
+        compra(jugador,sumaTotal);
+    }
+    else if ((coche < 1) || (coche > 6)){
         alert("numero ingresado no valido");
+    }else if(coche == 6){
+        alert("gracias por su compra");
     }
-} while (coche == 1,2,3,4,5);
+} while (coche != 6);
 
 
 
@@ -47,99 +73,19 @@ do {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-function Persona1 (nombre,apellido,edad,altura){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.edad = edad;
-    this.altura = altura;
-    this.saludar = () => console.log(`${this.nombre} te esta saludando.`);
-}
-*/ 
-
-/*
-function comprar(plata){
-    plata == coche
-    console.log(`compraste el siguiente ${coche}`)
-}*/
-
-
-/*const cristianoRonaldo ={
-    nombre : "Cristiano",
-    apellido: "Ronaldo",
-    edad: 32,
-    altura:2.00,
-}
-class Vehiculos{
-    constructor(marca,modelo,velocidad,peso){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.velocidad = velocidad;
-        this.peso = peso;
-    }
-}
-
-const auto1 = new Vehiculos ("bugatti","Chiron","407 Km/h",2700);
-const auto2 = new Vehiculos ("McLaren","MP412C","326 Km/h",1350);
-const auto3 = new Vehiculos ("ferrari","599 GTE","335 Km/h",1750);
-const auto4 = new Vehiculos ("lamborgini","aventador","350Km/h",1200);
-
-const cochera = [];
-let agregar;
-let suma
-do {
-    alert("ingrese un auto a su cochera sin que sobrepase los 400' kg")
-    agregar = prompt(`selecione la opcion de acuerdo al numero \n 1- auto1 \n 2- auto2 \n 3- auto3 \n 4- auto4 \n 5- finaizar`)
-
-}while ((agregar !== 1) & (agregar !== 2) & (agregar !== 3)& (agregar !== 4))
-switch (agregar) {
-    case 1:
-        if(cochera<=4000){
-            suma = cochera.push(auto1)
-            console.log(suma);
-        }
-        break;
-    case 2:
-        if (suma <= 4000){
-            suma = cochera.push(auto2)
-            console.log(suma);
-        }
-        break;
-    case 3:
-        if (suma <= 4000){
-            suma = cochera.push(auto3)
-            console.log(suma);
-        }
-        break;
-}
+/*let sumaTotal = autosCarrera.map(autosarray => autosarray.precio);
+let sumaTotal,total,saldo;
+        do {
+            sumaTotal = autosCarrera.map(autosarray => autosarray.precio);
+            total = console.log(sumaTotal.reduce((pre,act)=> pre + act,0));
+            jugador = parseFloat(prompt("ingrese el monto total para comprar todos"));
+            if (jugador == total) {
+                console.log("gracias por su compra");
+            }else if(jugador>total){
+                saldo = jugador - total ;
+                console.log(`su compra fue exitosa,le sobra$${saldo}`);
+            }else if (jugador<total){
+                console.log("el dinero ingresado no es suficiente para comprar a todos los autos");
+            }
+        } while (jugador == total);
 */
