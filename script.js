346610dbd8a8b4ea98f6529a7c58423de8af4d07
Eduardo Tolaba <eduardoetolaba@gmail.com>
2022-08-17 19:02:56 -0300

    agregado de libreria de toastify al ecommerc

--- script.js
+++ script.js
@@ -52,6 +52,19 @@ if (localStorage.getItem("changito")) {
 depositoUno.forEach((producto,indice) =>{
     document.getElementById(`producto${indice}`).children[1].children[4].addEventListener(`click`,()=>{
 
+        Toastify({
+            text: "Producto agregado al carrito",
+            duration: 5000,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to right, #F16529, #E44D26)",
+            },
+            onClick: function(){} // Callback after click
+        }).showToast();
+
         const agregados = new Herramientas(producto.nombre,producto.marca,producto.detalle,producto.precio,producto.img);
         changito.push(agregados);
         //guardo los datos en el localstorage
@@ -80,19 +93,9 @@ depositoUno.forEach((producto,indice) =>{
 
 
 
-
-
-
-
-
-
-
-
-
 //Este codigo nos permite ingresar productos desde javaScript y visualizarlo en el DOM.
 class Hogar{
-    constructor(id,nombre,detalle,precio,img){
-        this.id = id;
+    constructor(nombre,detalle,precio,img){
         this.nombre = nombre;
         this.detalle = detalle;
         this.precio = precio;
@@ -100,42 +103,70 @@ class Hogar{
     }
 }
 
-const tenaza = new Hogar (1,"Tenaza","acero forjado medio corte 22,5 cm",3170,"http://www.sidercon.com/imagenes/8201-572-tenaza-carpintero-200mm-bahco-541-d-200a.jpg");
-const pinza = new Hogar (2,"Pinza","de agarre medio cabo naranja reforzado",1250,"https://www.martinezescalada.com.ar/551-extra_large_default/pinza-universal-992801-7.jpg");
-const plomadin = new Hogar (3,"Plomada Albañil","peso de 300 gr toth industria nacional",870,"https://www.hierrosmaldonado.com.ar/1557-large_default/plomada-400-gr.jpg");
-const cuchara = new Hogar (4,"Cuchara Albañil","acero reforzada punta redonda profesional",650,"https://arcencohogar.vtexassets.com/arquivos/ids/300009-800-800?v=637665764479800000&width=800&height=800&aspect=true");
-const balde = new Hogar (5,"Balde","acero galbanizado de 15 lt ultra resistente",2100,"https://www.pronor.com.ar/images/PM-BALCHP1599587895908.jpg");
-const nivel = new Hogar (6,"Nivel","nivelador de mano a burbuja color amarillo",860,"https://es.cornilleau.com/4773-large_default/nivel-de-burbuja.jpg");
-const martillo = new Hogar(7,"Martillo","maza de acero 1000gr con cabo de madera",1230,"https://assets.tramontina.com.br/upload/tramon/imagens/GAR/40682008PDM001G.jpg");
-const tanza = new Hogar (8,"Tanza Albañil","color rojo de 0,80 mm X 100 mts",260,"https://www.pronor.com.ar/images/00000000RC-7553699874RC-75536.jpg");
+const tenaza = new Hogar ("Tenaza","acero forjado medio corte 22,5 cm",3170,"http://www.sidercon.com/imagenes/8201-572-tenaza-carpintero-200mm-bahco-541-d-200a.jpg");
+const pinza = new Hogar ("Pinza","de agarre medio cabo naranja reforzado",1250,"https://www.martinezescalada.com.ar/551-extra_large_default/pinza-universal-992801-7.jpg");
+const plomadin = new Hogar ("Plomada Albañil","peso de 300 gr toth industria nacional",870,"https://www.hierrosmaldonado.com.ar/1557-large_default/plomada-400-gr.jpg");
+const cuchara = new Hogar ("Cuchara Albañil","acero reforzada punta redonda profesional",650,"https://arcencohogar.vtexassets.com/arquivos/ids/300009-800-800?v=637665764479800000&width=800&height=800&aspect=true");
+const balde = new Hogar ("Balde","acero galbanizado de 15 lt ultra resistente",2100,"https://www.pronor.com.ar/images/PM-BALCHP1599587895908.jpg");
+const nivel = new Hogar ("Nivel","nivelador de mano a burbuja color amarillo",860,"https://es.cornilleau.com/4773-large_default/nivel-de-burbuja.jpg");
+const martillo = new Hogar("Martillo","maza de acero 1000gr con cabo de madera",1230,"https://assets.tramontina.com.br/upload/tramon/imagens/GAR/40682008PDM001G.jpg");
+const tanza = new Hogar ("Tanza Albañil","color rojo de 0,80 mm X 100 mts",260,"https://www.pronor.com.ar/images/00000000RC-7553699874RC-75536.jpg");
 
 const Plomeria = [tenaza,pinza,plomadin,cuchara,balde,nivel,martillo,tanza];
+
 const contenedorPlomeria = document.getElementById("cajaDos");
 
-Plomeria.forEach(cosas2 =>{
+Plomeria.forEach((cosas2,numero) =>{
     contenedorPlomeria.innerHTML +=`
     <div class="boxDos">
-        <div class="card"  ${cosas2.id}>
+        <div class="card"  id="elemento${numero}">
             <img src="${cosas2.img}" class="card-img-top" alt="imagen del producto">
             <div class="card-body">
                 <h4 class="card-title">${cosas2.nombre}</h4>
                 <p class="card-text">${cosas2.detalle}</p>
                 <h3 class="card-text">$${cosas2.precio}</h3>
-                <button class="btn btn-primary" id="agregar">agregar</button>
+                <button class="btn btn-primary">agregar</button>
             </div>
         </div>
     </div>`
 });
 
-
-
-
+Plomeria.forEach((productos,numero)=>{
+    //este codigo permite que al hacer click en agregar,sale una alerta 
+    document.getElementById(`elemento${numero}`).children[1].children[3].addEventListener(`click`,()=>{
+        Toastify({
+            text: "Producto agregado al carrito",
+            duration: 5000,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to right, #F16529, #E44D26)",
+            },
+            onClick: function(){} // Callback after click
+        }).showToast();
+    });
+});
 
 /*
-const pedidos = [];
-const carrito =document.getElementById("agregar");
 
-carrito.addEventListener(`click`,()=>{
-    const producto = [`${cosas2.nombre},${cosas2.precio},${cosas2.id}`];
-    pedidos.push(producto);
-})*/
+btnAgregar.addEventListener(`click`,()=>{
+    Toastify({
+        text: "This is a toast",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+    }).showToast();
+})
+
+
+*/
