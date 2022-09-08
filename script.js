b093a0e0bc31c4e02455c878e894e1cab823c4ed
Eduardo Tolaba <eduardoetolaba@gmail.com>
2022-09-08 19:59:05 -0300

    cambios en el js del carrito

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 const conteiner = document.getElementById("cajaUno");
 const conteinerChanguito = document.getElementById("conteiner-changuito");
 const botonVerCarrito = document.getElementById("boton-ver-carrito");
-<<<<<<< HEAD
-const elTotal = document.getElementById("precioTotal");
-=======
-const cajaTotal = document.getElementById("box-total");
->>>>>>> rama
 
 const changito = JSON.parse(localStorage.getItem('changito'))??[];
-
-async function arrayProductos () {
-    const traer = await fetch(`./JSON/datos.json`)
-    const consulta = await traer.json()
-    return consulta;
-}
 
 fetch(`./JSON/datos.json`)
 .then(response => response.json())
@@ -43,7 +32,7 @@ fetch(`./JSON/datos.json`)
             //muestra una alerta al presionar el boton agregar carrito.
             Toastify({
                 text: "Producto agregado al carrito",
-                duration: 3000,
+                duration: 5000,
                 close: true,
                 gravity: "bottom", // `top` or `bottom`
                 position: "right", // `left`, `center` or `right`
@@ -53,37 +42,17 @@ fetch(`./JSON/datos.json`)
                 },
                 onClick: function(){} // Callback after click
             }).showToast();
-            //este codigo permite ver,si el producto agregado existe agrego la cantidad en 1,sino lo crea.
-            if(changito.find(elem=>elem.nombre === producto.nombre)){
-                changito[indice].cantidad++
-            }else{
-                const agregadoChangito = {nombre:producto.nombre,precio:producto.precio,cantidad:1};
-                changito.push(agregadoChangito);
-            }
-            //guardo los datos en el array del localstorage
+    
+            //agarra los datos del producto y los almacena en una nueva arrays.
+            changito.push(producto);
+            //guardo los datos en el localstorage
             localStorage.setItem("changito",JSON.stringify(changito));
         });
     });  
 });
 
-<<<<<<< HEAD
-//funcion eliminar los productos del carrito
-function deleteProduct (){
-    document.getElementById(`producto${index}`).remove();
-    changito.splice(index,1);
-    localStorage.setItem("changito",JSON.stringify(changito));
-}
-
-let precioTotal = 0;
-=======
-
-//creo un let y que su valor inicial sea 0;
-let sumaTotal = 0;
-
->>>>>>> rama
 //pido los datos del localstorage,
 const pedirDatos = JSON.parse(localStorage.getItem("changito"));
-<<<<<<< HEAD
 botonVerCarrito.addEventListener(`click`,()=>{
     if(pedirDatos.length == 0){
         Swal.fire({
@@ -93,55 +62,32 @@ botonVerCarrito.addEventListener(`click`,()=>{
             imageWidth: 400,
             imageHeight: 300,
             imageAlt: 'Custom image',
-        })
+        });
     }else{
-        //este lo que hace es sumar el total de los precios y mostrarlo en el dom.
-        pedirDatos.forEach(cant =>{
-            cajaTotal.innerHTML = `
-            <div id="box-suma-children">
-                Precio Total es:$${sumaTotal+= cant.precio}
-            </div>
-            <button class="btn btn-danger" id="vaciar-carro">Vaciar carrito</button>
-            `
-            document.getElementById("vaciar-carro").addEventListener(`click`,()=>{
-                console.log("vaciar carrito")
-            })
-        })
-
         //muestra en el DOM los productos agregados al carrito.
         pedirDatos.forEach((guardado,index) =>{
-=======
-botonVerCarrito.addEventListener(`click`,/*async*/ ()=>{
-    pedirDatos.forEach(cant =>{
-        elTotal.innerHTML = `
-        <div>
-            precio total : ${precioTotal+= cant.precio}
-        </div>
-        `
-    })
-    //const suma = (pedirDatos.precio*pedirDatos.cantidad)
-    //const datosArray = await arrayProductos();
-    //muestra en el DOM los productos agregados al carrito.
-    pedirDatos.forEach((guardado,index) =>{
->>>>>>> paralela
             conteinerChanguito.innerHTML += `
             <div class="card text-dark bg-primary mb-3" style="max-width:450px;" id="producto${index}" >
-                    <div class="col-md-12">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${guardado.img}" class="img-fluid rounded-start" alt="">
+                    </div>
+                    <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">producto: ${guardado.nombre}</h5>
-                            <p class="card-text">$${guardado.precio}</p>
-                            <p class="card-text">cantidad: ${guardado.cantidad}</p>
+                            <p class="card-text">$ ${guardado.precio}</p>
                             <button class="btn btn-info">comprar</button>
                             <button class="btn btn-danger">eliminar</button>
                         </div>
                     </div>
+                </div>
             </div>
             `
-    });
+        });
         
         //este codigo permite comprar productos del carrito 
-    pedirDatos.forEach((producto,index)=>{
-            document.getElementById(`producto${index}`).children[0].children[0].children[3].addEventListener(`click`,()=>{
+        pedirDatos.forEach((producto,index)=>{
+            document.getElementById(`producto${index}`).children[0].children[1].children[0].children[2].addEventListener(`click`,()=>{
                 Swal.fire({
                     position: 'center',
                     icon: 'success',
@@ -153,24 +99,11 @@ botonVerCarrito.addEventListener(`click`,/*async*/ ()=>{
                 changito.splice(index,1);
                 localStorage.setItem("changito",JSON.stringify(changito));
             })
-            document.getElementById(`producto${index}`).children[0].children[0].children[4].addEventListener(`click`,()=>{
+            document.getElementById(`producto${index}`).children[0].children[1].children[0].children[3].addEventListener(`click`,()=>{
                 document.getElementById(`producto${index}`).remove();
                 changito.splice(index,1);
                 localStorage.setItem("changito",JSON.stringify(changito));
-<<<<<<< HEAD
             });
         });
     };
 });
-=======
-            })
-    });
-
-});
-
-/*
-<div class="col-md-4">
-        <img src="" class="img-fluid rounded-start" alt="">
-</div>
-*/
->>>>>>> paralela
